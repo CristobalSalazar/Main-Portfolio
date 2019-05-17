07fd1dbcbbc8cf10bd948e54fe73c4f005b05c57
CristobalSalazar <chris.salza@gmail.com>
2019-05-16 21:29:25 -0400

    added social media links

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,7 +1,26 @@
 var aboutLink = document.getElementById("about");
 var navLinks = document.getElementsByClassName("navLinks");
+var introContainer = document.getElementById("intro");
+var headshot = document.getElementById("headshot");
 
 function scrollToAbout() 
 {
     aboutLink.scrollIntoView( { behavior: "smooth", block: "start"} );
 }
+
+function resizeImg() {
+    
+    var img = new Image();
+    img.src = headshot.src;
+
+    console.log(img.width);
+    console.log(img.height);
+
+    if (window.innerWidth <= img.width/2)
+    {
+        console.log("overflow");
+    }
+}
+
+window.onload = resizeImg;
+window.onresize = resizeImg;
