97e90b6b35cb3f48e3bf4e6674f79f102f6301ea
CristobalSalazar <chris.salza@gmail.com>
2019-05-18 19:39:28 -0400

    added dropdown to navbar and updated title image for mobile

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,26 +1,80 @@
 var aboutLink = document.getElementById("about");
-var introContainer = document.getElementById("intro");
-var headshot = document.getElementById("headshot");
+var open = false;
 
 function scrollToAbout() 
 {
     aboutLink.scrollIntoView( { behavior: "smooth", block: "start"} );
 }
 
-function getRelativeWidth(height, img) {
-    const aspectRatio = img.width / img.height;
-    return height * aspectRatio;
+function closeDropdown() {
+
+    let navbarElements = document.getElementsByClassName("navbarElement");
+    let navLinks = document.getElementsByClassName("navLink");
+
+    for (let i = 0; i < navbarElements.length; i++)
+    {
+        let navbarElement = navbarElements.item(i);
+        navbarElement.style.display = ("none");
+    }
+    
+    for (let i = 0; i < navLinks.length; i++)
+    {
+        let navLink = navLinks.item(i);
+        navLink.style.display = ("none");
+    }
+
+    open = false;
 }
 
-function getRelativeHeight(width, img) {
-    const aspectRatio = img.height / img.width;
-    return width * aspectRatio;
+function dropdownToggle() {
+
+    open = !open;
+    let navbarElements = document.getElementsByClassName("navbarElement");
+    let navLinks = document.getElementsByClassName("navLink");
+    let activePages = document.getElementsByClassName("activePage");
+
+    if (open)
+    {
+        for (let i = 0; i < activePages.length; i++)
+        {
+            activePages.item(i).className = "navLink";
+        }
+        
+        for (let i = 0; i < navbarElements.length; i++) {
+
+            let navbarElement = navbarElements.item(i);
+            navbarElement.style.display = ("block");
+            
+            if (i == 0) {
+                navbarElement.style.margin = "48px 0px 0px 0px";
+            }
+            else {
+                navbarElement.style.margin = "12px 0px 0px 0px";
+            }
+        }
+        
+        for (let i = 0; i < navLinks.length; i++)
+        {
+            let navLink = navLinks.item(i);
+            navLink.style.display = ("block");
+            navLink.style.padding = ("12px");
+        }
+    }
+
+    else {
+        
+        for (let i = 0; i < navbarElements.length; i++)
+        {
+            let navbarElement = navbarElements.item(i);
+            navbarElement.style.display = ("none");
+        }
+        
+        for (let i = 0; i < navLinks.length; i++)
+        {
+            let navLink = navLinks.item(i);
+            navLink.style.display = ("none");
+        }
+    }
+
+    window.onresize = closeDropdown;
 }
-
-function resizeImg() {
-
-
-}
-
-window.onload = resizeImg;
-window.onresize = resizeImg;
