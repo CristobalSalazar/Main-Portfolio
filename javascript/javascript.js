c0fc06dc8aa8c6ef39acc895286be0316ceb99cc
CristobalSalazar <chris.salza@gmail.com>
2019-05-18 21:23:58 -0400

    fized bug in mobile nav

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -21,29 +21,27 @@ function dropdownToggle() {
     let navbarElements = document.getElementsByClassName("navbarElement");
     let activePage = document.getElementsByClassName("activePage").item(0);
     
-
     open = !open;
     console.log("open " + open);
     
-    if (open)
-    {
+    if (open) {
         mobileNav.style.opacity = "0.5";
         if (activePage != null)
         {   
             activePage.className = "navLink";
         }
+        navLayout.style.marginTop = "32px";
         navLayout.style.display = "block";
         for (let i = 0; i < navLinks.length; i++) {
             let navLink = navLinks.item(i);
             navLink.style.display = "block";
-            navLink.style.margin = "36px 0 0 0";
+            navLink.style.padding = "18px";
         }
         for (let i = 0; i < navbarElements.length; i++) {
             let navElement = navbarElements.item(i);
             navElement.style.display = "block";
         }
     }
-    
     else {
         mobileNav.style.opacity = "1";
         navLayout.style.display = "none";
