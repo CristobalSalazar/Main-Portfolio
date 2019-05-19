f1440e012d470c5c7caa3344a26f162fb17347f2
CristobalSalazar <chris.salza@gmail.com>
2019-05-18 21:01:59 -0400

    improved navbar navigation for mobile

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -8,73 +8,44 @@ function scrollToAbout()
 
 function closeDropdown() {
 
-    let navbarElements = document.getElementsByClassName("navbarElement");
-    let navLinks = document.getElementsByClassName("navLink");
-
-    for (let i = 0; i < navbarElements.length; i++)
-    {
-        let navbarElement = navbarElements.item(i);
-        navbarElement.style.display = ("none");
-    }
-    
-    for (let i = 0; i < navLinks.length; i++)
-    {
-        let navLink = navLinks.item(i);
-        navLink.style.display = ("none");
-    }
-
+    let navLayout = document.getElementById("navLayout");
+    console.log(navLayout.parentElement);  
     open = false;
+    
 }
 
 function dropdownToggle() {
+    let mobileNav = document.getElementById("mobileNav");
+    let navLayout = document.getElementById("navLayout");
+    let navLinks = document.getElementsByClassName("navLink");
+    let navbarElements = document.getElementsByClassName("navbarElement");
+    let activePage = document.getElementsByClassName("activePage").item(0);
+    
 
     open = !open;
-    let navbarElements = document.getElementsByClassName("navbarElement");
-    let navLinks = document.getElementsByClassName("navLink");
-    let activePages = document.getElementsByClassName("activePage");
-
+    console.log("open " + open);
+    
     if (open)
     {
-        for (let i = 0; i < activePages.length; i++)
-        {
-            activePages.item(i).className = "navLink";
+        mobileNav.style.opacity = "0.5";
+        if (activePage != null)
+        {   
+            activePage.className = "navLink";
         }
-        
+        navLayout.style.display = "block";
+        for (let i = 0; i < navLinks.length; i++) {
+            let navLink = navLinks.item(i);
+            navLink.style.display = "block";
+            navLink.style.margin = "36px 0 0 0";
+        }
         for (let i = 0; i < navbarElements.length; i++) {
-
-            let navbarElement = navbarElements.item(i);
-            navbarElement.style.display = ("block");
-            
-            if (i == 0) {
-                navbarElement.style.margin = "48px 0px 0px 0px";
-            }
-            else {
-                navbarElement.style.margin = "12px 0px 0px 0px";
-            }
-        }
-        
-        for (let i = 0; i < navLinks.length; i++)
-        {
-            let navLink = navLinks.item(i);
-            navLink.style.display = ("block");
-            navLink.style.padding = ("12px");
+            let navElement = navbarElements.item(i);
+            navElement.style.display = "block";
         }
     }
-
+    
     else {
-        
-        for (let i = 0; i < navbarElements.length; i++)
-        {
-            let navbarElement = navbarElements.item(i);
-            navbarElement.style.display = ("none");
-        }
-        
-        for (let i = 0; i < navLinks.length; i++)
-        {
-            let navLink = navLinks.item(i);
-            navLink.style.display = ("none");
-        }
+        mobileNav.style.opacity = "1";
+        navLayout.style.display = "none";
     }
-
-    window.onresize = closeDropdown;
 }
