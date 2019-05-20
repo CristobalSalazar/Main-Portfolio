97332eaab8662f0f0ef65e59208276f978627a6b
CristobalSalazar <chris.salza@gmail.com>
2019-05-20 17:42:54 -0400

    cleaned up unused functions

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -5,45 +5,18 @@ function scrollToAbout()
 {
     aboutLink.scrollIntoView( { behavior: "smooth", block: "start"} );
 }
-
-function closeDropdown() {
-
-    let navLayout = document.getElementById("navLayout");
-    console.log(navLayout.parentElement);  
-    open = false;
-    
-}
-
-function dropdownToggle() {
+function dropdownToggle()
+{
     let mobileNav = document.getElementById("mobileNav");
-    let navLayout = document.getElementById("navLayout");
-    let navLinks = document.getElementsByClassName("navLink");
-    let navbarElements = document.getElementsByClassName("navbarElement");
-    let activePage = document.getElementsByClassName("activePage").item(0);
+    let navLayout = document.getElementsByClassName("navLayout").item(0);
     
     open = !open;
-    console.log("open " + open);
     
     if (open) {
-        mobileNav.style.opacity = "0.5";
-        if (activePage != null)
-        {   
-            activePage.className = "navLink";
-        }
-        navLayout.style.marginTop = "32px";
-        navLayout.style.display = "block";
-        for (let i = 0; i < navLinks.length; i++) {
-            let navLink = navLinks.item(i);
-            navLink.style.display = "block";
-            navLink.style.padding = "18px";
-        }
-        for (let i = 0; i < navbarElements.length; i++) {
-            let navElement = navbarElements.item(i);
-            navElement.style.display = "block";
-        }
+        navLayout.style.display = "inline";        
     }
     else {
-        mobileNav.style.opacity = "1";
         navLayout.style.display = "none";
+        mobileNav.style.opacity = "1";
     }
 }
