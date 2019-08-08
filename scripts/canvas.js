7b1de56f060e3bd4e383279a06eca24e7db372c1
chris <chris.salza@gmail.com>
2019-08-07 22:19:13 -0400

    simplified title, removed canvas

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,58 +1,60 @@
 (function () {
   "use strict"
+
   const canvas = document.getElementById("intro-canvas");
   const intro = document.getElementById('intro');
   const dpr = window.devicePixelRatio;
   const context = canvas.getContext("2d");
   const goldenRatio = 1.61803398875;
 
-  // ------ Sizing ------
-  function setCanvasSize() {
-    const canvasHeight = canvas.clientHeight
-    const canvasWidth = canvas.clientWidth;
-
-    canvas.setAttribute("height", canvasHeight * dpr);
-    canvas.setAttribute("width", canvasWidth * dpr);
-  }
-
   // ------ Circle ------
   function Circle() {
     this.init();
   }
   Circle.prototype.init = function () {
-    let minRadius = 18 * (goldenRatio - 1);
-    let maxRadius = 18;
-
-    minRadius = 16;
-    maxRadius = 16;
-    let xOffset = canvas.width / 4;
-    // let yvel = randomRange(-6, -12);
+    // config
+    let minRadius = 24 * (goldenRatio - 1);
+    let maxRadius = 24;
     let yvel = 0;
-    // let xvel = (Math.random() - 0.5);
     let xvel = 0;
+    let rndPosFactor = 24;
     let shrinkRate = Math.random();
-    let fadeRate = Math.random();
+    let fadeRate = Math.random() / 0.05;
 
-    if (breakpoints.sm) {
 
-      xOffset = 0;
-      interval = 0;
-    }
-    this.xOffset = xOffset;
     this.radius = randomRange(minRadius, maxRadius) * dpr;
     this.shrinkRate = shrinkRate;
-    // this.x = randomRange(this.radius + interval, canvas.width - interval - this.radius);
-    // this.x += xOffset;
-    this.x = mouse.x + (Math.random() - 0.5) * 25;
-    // this.y = -this.radius;
-    // this.y = this.radius + canvas.height - Math.random() * 2;
-    this.y = mouse.y + (Math.random() - 0.5) * 25;
+    this.fadeRate = fadeRate;
+
+    this.x = mouse.x + (Math.random() - 0.5) * rndPosFactor;
+    this.y = mouse.y + (Math.random() - 0.5) * rndPosFactor;
+
     this.yvel = yvel;
     this.xvel = xvel;
-    this.fadeRate = fadeRate;
+
     this.opacity = 1;
     this.color = new Color(0, 255, 255);
   }
+  Circle.prototype.update = function () {
+    const xForce = 0;
+    const yForce = 0.0985;
+    const shrinkDelta = 0.3;
+    const above = this.y + this.radius < 0;
+    const below = this.y - this.radius > canvas.height;
+    const isBlack = this.color.r < 1 && this.color.g < 1 && this.color.b < 1;
+    this.shrinkRate -= shrinkDelta;
+    this.color = colorLerp(this.color, new Color(255, 255, 0), 0.05)
+    this.x += this.xvel;
+    this.y += this.yvel;
+    this.yvel += yForce * dpr;
+    this.xvel += xForce * dpr;
+    this.radius += this.shrinkRate * dpr;
+    this.opacity = this.opacity <= 0 ? (this.opacity = 0) : (this.opacity -= this.fadeRate);
+    // reset
+    if (this.radius <= 0 || this.opacity <= 0 || above || below || isBlack) {
+      this.init()
+    }
+  };
   Circle.prototype.draw = function () {
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
@@ -62,32 +64,7 @@
     context.closePath();
     context.globalAlpha = 1;
   };
-  Circle.prototype.update = function () {
-    const xForce = 0;
-    const yForce = 0.985;
-    const shrinkDelta = 0.02;
-    const above = this.y + this.radius < 0;
-    const below = this.y - this.radius > canvas.height;
-    const isBlack = this.color.r < 5 && this.color.g < 5 && this.color.b < 5;
-    this.shrinkRate += shrinkDelta;
-    this.color = colorLerp(this.color, new Color(255, 255, 0), 0.05)
-    this.x += this.xvel;
-    this.y += this.yvel;
-    this.yvel += yForce * dpr;
-    this.xvel += xForce * dpr;
-    this.radius -= this.shrinkRate * dpr;
-    // this.x = lerp(this.x, canvas.width / 2 + this.xOffset, 0.02);
-
-    if (below) {
-      this.yvel = -this.yvel;
-    }
-
-    this.opacity = this.opacity <= 0 ? (this.opacity = 0) : (this.opacity -= this.fadeRate);
-    if (this.radius <= 0 || this.opacity <= 0 || above || below || isBlack) {
-      this.init()
-    }
-  };
-  Circle.prototype.createCircles = function (density) {
+  Circle.prototype.getCircles = function (density) {
     const circles = [];
     for (let i = 0; i < density; i++) {
       const circle = new Circle();
@@ -102,6 +79,7 @@
     this.y = Number.MAX_SAFE_INTEGER;
   };
   Mouse.prototype.getCoords = function (e, isTouch = false) {
+    console.log('getCoords');
     let offset = canvas.getBoundingClientRect().top
     if (isTouch) {
       this.x = e.touches[0].clientX * dpr;
@@ -111,23 +89,18 @@
       this.y = e.clientY - offset * dpr;
     }
   };
-  Mouse.prototype.reset = function () {
+  Mouse.prototype.reset = function (e) {
+    console.log('reseting')
     this.x = Number.MAX_SAFE_INTEGER;
     this.y = Number.MAX_SAFE_INTEGER;
   };
-  const mouse = new Mouse();
-
-  window.addEventListener("mouseleave", mouse.reset);
-  window.addEventListener("mousemove", e => mouse.getCoords(e));
-
-
-  if (breakpoints.md) {
-    canvas.addEventListener('touchstart', e => mouse.getCoords(e, true));
-    canvas.addEventListener("touchmove", e => mouse.getCoords(e, true));
-    canvas.addEventListener('touchend', mouse.reset)
-  }
 
   // ------ Utility ------
+  function setCanvasSize() {
+    canvas.setAttribute("height", canvas.clientHeight * dpr);
+    canvas.setAttribute("width", canvas.clientWidth * dpr);
+  }
+
   function lerp(a, b, t) {
     return (1 - t) * a + t * b;
   }
@@ -170,20 +143,33 @@
       posy = 0;
     }
     context.clearRect(0, 0, canvas.width, canvas.height);
+    circles.sort((x, y) => {
+      x.radius > y.radius;
+    })
     for (let circle of circles) {
       circle.update();
       circle.draw();
     }
-
-
     requestAnimationFrame(render);
   }
 
   // ------ Execution ------
   setCanvasSize()
-  if (!breakpoints.sm) {
-    window.onresize = setCanvasSize;
+
+  const mouse = new Mouse();
+  const circles = Circle.prototype.getCircles(200);
+
+  if (breakpoints.md) {
+    canvas.addEventListener('touchstart', e => mouse.getCoords(e, true));
+    canvas.addEventListener("touchmove", e => mouse.getCoords(e, true));
+    canvas.addEventListener('touchend', e => mouse.reset(e))
+  } else {
+    window.addEventListener("mouseleave", mouse.reset);
+    window.addEventListener("mousemove", e => mouse.getCoords(e));
+    window.addEventListener('click', mouse.reset);
   }
-  const circles = Circle.prototype.createCircles(100);
+  window.onresize = setCanvasSize;
+  console.log(circles.length);
+
   render();
 })();
