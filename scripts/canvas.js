bff7b1197f8bf063334e5f4e1653261e6b346106
chris <chris.salza@gmail.com>
2019-08-12 15:24:19 -0400

    added bio text

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,8 +1,6 @@
 (function () {
   "use strict"
-
   const canvas = document.getElementById("intro-canvas");
-  const intro = document.getElementById('intro');
   const dpr = window.devicePixelRatio;
   const context = canvas.getContext("2d");
   const goldenRatio = 1.61803398875;
@@ -11,37 +9,34 @@
   function Circle() {
     this.init();
   }
+
   Circle.prototype.init = function () {
-    // config
     let minRadius = 24 * (goldenRatio - 1);
     let maxRadius = 24;
     let yvel = 0;
-    let xvel = 0;
-    let rndPosFactor = 24;
-    let shrinkRate = Math.random();
-    let fadeRate = Math.random() / 0.05;
-
+    let xvel = (Math.random() - 0.5) * goldenRatio;
+    let rndPosFactor = 5;
+    let shrinkRate = Math.random() / 2;
+    let fadeRate = Math.random() * 10;
 
     this.radius = randomRange(minRadius, maxRadius) * dpr;
     this.shrinkRate = shrinkRate;
     this.fadeRate = fadeRate;
-
     this.x = mouse.x + (Math.random() - 0.5) * rndPosFactor;
     this.y = mouse.y + (Math.random() - 0.5) * rndPosFactor;
-
     this.yvel = yvel;
     this.xvel = xvel;
-
     this.opacity = 1;
-    this.color = new Color(0, 255, 255);
+    this.color = new Color(255, 255 / 2, 255 / 2);
   }
   Circle.prototype.update = function () {
     const xForce = 0;
-    const yForce = 0.0985;
-    const shrinkDelta = 0.3;
+    const yForce = 0.985;
+    const shrinkDelta = goldenRatio / 10;
     const above = this.y + this.radius < 0;
     const below = this.y - this.radius > canvas.height;
     const isBlack = this.color.r < 1 && this.color.g < 1 && this.color.b < 1;
+
     this.shrinkRate -= shrinkDelta;
     this.color = colorLerp(this.color, new Color(255, 255, 0), 0.05)
     this.x += this.xvel;
@@ -49,7 +44,8 @@
     this.yvel += yForce * dpr;
     this.xvel += xForce * dpr;
     this.radius += this.shrinkRate * dpr;
-    this.opacity = this.opacity <= 0 ? (this.opacity = 0) : (this.opacity -= this.fadeRate);
+    this.opacity -= this.fadeRate;
+    this.opacity = lerp(this.opacity, 0, 0.1);
     // reset
     if (this.radius <= 0 || this.opacity <= 0 || above || below || isBlack) {
       this.init()
@@ -129,23 +125,12 @@
     window.webkitRequestAnimationFrame ||
     window.msRequestAnimationFrame;
 
-  let posy = 0;
-
   function render() {
     if (canvas.getBoundingClientRect().bottom < 0) {
       requestAnimationFrame(render);
       return;
     }
-    intro.style.backgroundPositionY = -posy + 'px';
-    canvas.style.backgroundPositionY = -posy / 2 + 'px';
-    posy += 10;
-    if (posy < -canvas.height) {
-      posy = 0;
-    }
     context.clearRect(0, 0, canvas.width, canvas.height);
-    circles.sort((x, y) => {
-      x.radius > y.radius;
-    })
     for (let circle of circles) {
       circle.update();
       circle.draw();
@@ -155,21 +140,15 @@
 
   // ------ Execution ------
   setCanvasSize()
-
+  // events
   const mouse = new Mouse();
   const circles = Circle.prototype.getCircles(200);
-
-  if (breakpoints.md) {
-    canvas.addEventListener('touchstart', e => mouse.getCoords(e, true));
-    canvas.addEventListener("touchmove", e => mouse.getCoords(e, true));
-    canvas.addEventListener('touchend', e => mouse.reset(e))
-  } else {
-    window.addEventListener("mouseleave", mouse.reset);
-    window.addEventListener("mousemove", e => mouse.getCoords(e));
-    window.addEventListener('click', mouse.reset);
-  }
-  window.onresize = setCanvasSize;
-  console.log(circles.length);
+  canvas.addEventListener("touchstart", e => mouse.getCoords(e, true));
+  canvas.addEventListener("touchmove", e => mouse.getCoords(e, true));
+  canvas.addEventListener("touchend", e => mouse.reset(e))
+  window.addEventListener("mouseleave", mouse.reset);
+  window.addEventListener("mousemove", e => mouse.getCoords(e));
+  window.addEventListener("resize", setCanvasSize);
 
   render();
 })();
